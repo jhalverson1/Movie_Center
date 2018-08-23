11aceaa1d5af30c9ca6998cb3d2e391508aaf204
jonhalverson1 <jonhalverson9@gmail.com>
2018-08-22 18:26:07 -0700

    Started details page layout

--- js/movie_info.js
+++ js/movie_info.js
@@ -7,11 +7,21 @@ $(document).ready(function(){
 
     // take the movie_id that was stored in the local storage
     var movie_id = localStorage.movie_id;
-    var movie_title;
+
+    // Movie Details
+    var Movie_Details = {
+      title : "title",
+      year : "0000",
+      poster_path: "poster_path",
+      overview: "overview"
+    };
+
+
 
     // $("#movie_title").text(localStorage.movie_id);
 
     var movie = tmdb.movies.getById({"id":movie_id }, id_search_successCB, id_search_errorCB)
+
 
     // Function is called when the movie is found successfully
     function id_search_successCB(data) {
@@ -19,13 +29,36 @@ $(document).ready(function(){
         // Object created from json
         resultJSON = JSON.parse(data);
 
-        console.log("Success callback");
-        console.log(resultJSON);
+        generate_details(resultJSON);
+        // console.log("Success callback");
+        // console.log(resultJSON);
     };
 
     // Function is called when the movie is not found successfully
     function id_search_errorCB(data) {
         console.log("Error callback: " + data);
     };
+
+    function generate_details(resultJSON){
+        console.log(resultJSON);
+
+        // Set movie variables
+        Movie_Details.title = resultJSON.original_title;
+        Movie_Details.year = resultJSON.release_date.substring(0, 4);
+        Movie_Details.poster_path = resultJSON.poster_path;
+        Movie_Details.overview = resultJSON.overview;
+
+        // Load movie details onto the page
+        load_page_details();
+    }
+
+    // Display Movie Info
+    function load_page_details() {
+        $("#movie_title").text(Movie_Details.title);
+        $("#movie_year").text(Movie_Details.year);
+        document.getElementById("movie_poster").src = baseimg + Movie_Details.poster_path;
+        $("#movie_overview").text(Movie_Details.overview);
+
+    }
 
 });
