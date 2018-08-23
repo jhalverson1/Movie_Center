e97419cd767cfcf6c20c7d03c5e38bd56ccba69a
jonhalverson1 <jonhalverson9@gmail.com>
2018-08-23 15:08:22 -0700

    Updated home search page design

--- js/app.js
+++ js/app.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
 
     var api_key = config.TMDB_API;
-    var baseimg = "https://image.tmdb.org/t/p/original";
+    var baseimg = "https://image.tmdb.org/t/p/w185";
     var tmdb = theMovieDb;
     var resultJSON = {};
+
+    // Allows user to hit enter in text box to submit search
+   document.getElementById('movie_search_text').onkeypress=function(e){
+      if(e.keyCode==13){
+        document.getElementById('movie_search_btn').click();
+      }
+    }
 
   // When the user searches for a movie
 	 $("#movie_search_btn").click(function(){
@@ -21,11 +28,35 @@ $(document).ready(function(){
         for (i = 0; i < resultJSON.results.length; i++) {
             console.log(resultJSON.results[i].id);
             var current_id = resultJSON.results[i].id;
-            $("#newResultsList").append("<a href='movie_info.html' class='list-group-item list-group-item-action' id ='"
+            $("#newResultsList").append(
+                        // Clickable link using anchor element
+                        "<a href='movie_info.html' class='list-group-item list-group-item-action search_result_item' id ='"
                         + current_id + "' onClick='segue_click(this.id)'>"
-                        + resultJSON.results[i].title + ", "
-                        + resultJSON.results[i].release_date.substring(0, 4)
-                        + "</a>");
+                        + "<div class='container-fluid'>"
+                        + "<div class='row'>"
+
+                        // Movie Poster
+                        + "<div class='col-sm-1 poster_thumbnail'>"
+                        + "<img class='rounded' id='movie_poster' src='" + baseimg + resultJSON.results[i].poster_path + "' alt='movie_poster'>"
+                        + "</div>"
+
+                        // title and release year
+                        + "<div class='col-sm-5'>"
+                        + "<h3 class='title_header'>" + resultJSON.results[i].title + "</h3>"
+                        + "<p class='year_text'>" + resultJSON.results[i].release_date.substring(0, 4) + "</p>"
+                        + "</div>"
+
+                        // release year
+                        + "<div class='col-sm-6'>"
+                        + "</div>"
+
+                        + "</div>"
+                        + "</div>"
+                        // Title and Release Year
+                        // + resultJSON.results[i].title
+                        // + resultJSON.results[i].release_date.substring(0, 4)
+                        + "</a>"
+                      );
 
         }
     };
@@ -55,10 +86,3 @@ function segue_click(clicked_id) {
     localStorage.movie_id=clicked_id;
 
 };
-
-// Allows user to hit enter in text box to submit search
-// document.getElementById('movie_search_text').onkeypress=function(e){
-//     if(e.keyCode==13){
-//         document.getElementById('movie_search_btn').click();
-//     }
-// }
