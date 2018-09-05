5f89d8c915cac75ae9ae7d38663aaa495336c86b
jonhalverson1 <jonhalverson9@gmail.com>
2018-09-05 16:47:41 -0700

    added ability to add lists by genres on browse page

--- js/browse.js
+++ js/browse.js
@@ -6,9 +6,27 @@ $(document).ready(function() {
 
 
   // Number of pages as input
-  load_movies(4, "now_playing");
   load_movies(4, "popular");
   load_movies(4, "top_rated");
+
+  load_genre(4, "drama");
+
+  function load_genre(pages, genre) {
+    if (genre == "drama")
+    {
+      var current_page = 1;
+
+      while (current_page <= pages) {
+        console.log(current_page);
+        tmdb.discover.getMovies({"with_genres": "18", "page": current_page}, function(data) {
+          var resultJSON = JSON.parse(data);
+          load_carousel(resultJSON.results, "drama")
+        }, errorCB);
+
+        current_page++;
+      }
+    }
+  }
 
 
   // Function is called when movies successfully loaded
@@ -29,28 +47,7 @@ $(document).ready(function() {
   // Netflix style carousel
   function load_carousel(movies, category) {
 
-    if (category == "now_playing")
-    {
-      Object.keys(movies).forEach(function(index) {
-          var current_id = movies[index].id;
-
-          // Cronstruct full image path
-          var full_path;
-          if (movies[index].poster_path) {
-            full_path = baseimg_w185 + movies[index].poster_path;
-          } else {
-            full_path = "images/poster_unavailable.jpg";
-          }
-
-          $('#now_playing_div').append(
-            "<div class='list-group-item'>      \
-                <a href='movie_info.html' class='list-group-item list-group-item-action search_result_item' id ='" + current_id + "' onClick='segue_click(this.id)'> \
-                <img class='rounded item_img' src='" + full_path + "'>    \
-            <div>"
-          );
-      });
-    }
-    else if (category == "popular")
+    if (category == "popular")
     {
       Object.keys(movies).forEach(function(index) {
           var current_id = movies[index].id;
@@ -92,6 +89,27 @@ $(document).ready(function() {
           );
       });
     }
+    else if (category == "drama")
+    {
+      Object.keys(movies).forEach(function(index) {
+          var current_id = movies[index].id;
+
+          // Cronstruct full image path
+          var full_path;
+          if (movies[index].poster_path) {
+            full_path = baseimg_w185 + movies[index].poster_path;
+          } else {
+            full_path = "images/poster_unavailable.jpg";
+          }
+
+          $('#drama_div').append(
+            "<div class='list-group-item'>      \
+                <a href='movie_info.html' class='list-group-item list-group-item-action search_result_item' id ='" + current_id + "' onClick='segue_click(this.id)'> \
+                <img class='rounded item_img' src='" + full_path + "'>    \
+            <div>"
+          );
+      });
+    }
 
 
   }
@@ -99,21 +117,7 @@ $(document).ready(function() {
   // Load list of now_playing movies
   function load_movies(pages, category) {
 
-    if (category == "now_playing")
-    {
-      var current_page = 1;
-
-      while (current_page <= pages) {
-        tmdb.movies.getNowPlaying({"region": "US", "page": current_page}, function(data) {
-          // Object created from json
-          var resultJSON = JSON.parse(data);
-          load_carousel(resultJSON.results, category)
-        }, errorCB)
-
-        current_page++;
-      }
-    }
-    else if (category == "popular")
+    if (category == "popular")
     {
 
       var current_page = 1;
