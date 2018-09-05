75ce2acbb044c073dd34416dafe1bc18e95bf2c8
jonhalverson1 <jonhalverson9@gmail.com>
2018-09-05 00:16:37 -0700

    Carousel made for popular movies

--- js/browse.js
+++ js/browse.js
@@ -1,56 +1,71 @@
 $(document).ready(function() {
   var api_key = config.TMDB_API;
-  var baseimg = "https://image.tmdb.org/t/p/w300";
+  var baseimg_w185 = "https://image.tmdb.org/t/p/w185";
+  var baseimg_original = "https://image.tmdb.org/t/p/original";
   var tmdb = theMovieDb;
 
 
   // Number of pages as input
-  load_popular_movies(5);
-
-  function load_popular_movies(pages) {
-
-    var current_page = 1;
-
-    while (current_page <= pages) {
-      console.log("page: " + current_page);
-      tmdb.movies.getNowPlaying({"region": "US", "page": current_page}, successCB, errorCB)
-
-      current_page++;
-    }
+  load_popular_movies(2);
 
 
-  }
   // Function is called when movies successfully loaded
   function successCB(data) {
-
       // Object created from json
       var resultJSON = JSON.parse(data);
-      generate_list(resultJSON.results)
+      load_carousel(resultJSON.results)
   };
+
 
   // Function is called when movies are not successfully loaded
   function errorCB(data) {
       console.log("Error callback: " + data);
   };
 
-  function generate_list(movies) {
 
-    for (i = 0; i < movies.length; i++) {
-      console.log("Movie: " + movies[i]);
-      // Check and see if movie has a poster poster available
-      var full_image_url;
-      if (movies[i].poster_path) {
-        full_image_url = baseimg + movies[i].poster_path;
-      } else {
-        full_image_url = "images/poster_unavailable.jpg";
-      }
+  // Netflix style carousel
+  function load_carousel(movies) {
 
-      $("#movie_display_div").append(
-        "<div class='col-lg-2'>                                             \
-            <img class='poster_thumbnail' src='" + full_image_url + "' alt='movie poster'> \
-            <div>" + movies[i].title + "</div>                              \
-        </div>"
-      )
+    // $('#hold_movies').append("<div class='list-group' id='popular_movies'></div>");
+    console.log(movies);
+    Object.keys(movies).forEach(function(index) {
+        var current_id = movies[index].id;
+
+        // Cronstruct full image path
+        var full_path;
+        if (movies[index].poster_path) {
+          full_path = baseimg_w185 + movies[index].poster_path;
+        } else {
+          full_path = "images/poster_unavailable.jpg";
+        }
+
+        $('#hold_movies').append(
+          "<div class='list-group-item'>      \
+              <a href='movie_info.html' class='list-group-item list-group-item-action search_result_item' id ='" + current_id + "' onClick='segue_click(this.id)'> \
+              <img class='rounded item_img' src='" + full_path + "'>    \
+          <div>"
+        );
+    });
+
+  }
+
+  // Load list of popular movies
+  function load_popular_movies(pages) {
+
+    var current_page = 1;
+
+    while (current_page <= pages) {
+      tmdb.movies.getNowPlaying({"region": "US", "page": current_page}, successCB, errorCB)
+
+      current_page++;
     }
   }
+
 });
+
+function segue_click(clicked_id) {
+    console.log("segue");
+    // save clicked movie id to local storage for later use
+    localStorage.movie_id=clicked_id;
+
+};
